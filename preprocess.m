4bd067495592a126b1c0ce1efbfd5ba430f2d912
prm14 <prm@datrain.cn>
2015-11-02 13:29:20 +0800

    a

--- preprocess.m
+++ preprocess.m
@@ -19,16 +19,11 @@ end
 title('输入：电机电压'),xlabel('时间/s');ylabel('电压');
 subplot(212);hold on;
 for i1=1:length(t2)-1
-    plot(t2(i1:i1+1),[v2(i1) v2(i1)],'r');
-    plot([t2(i1+1) t2(i1+1)],v2(i1:i1+1),'r');
+    plot(t2(i1:i1+1),[v2(i1) v2(i1)],'b');
+    plot([t2(i1+1) t2(i1+1)],v2(i1:i1+1),'b');
 end
 title('输出：液面高度'),xlabel('时间/s');ylabel('液面高度');
-%% 截取输入电压的有效数据片段所在区间youxiao，并确定输出稳态值y0
-%15s31
-youxiao=4:57;
-y0=160;
-
-%50s15
+%% 观察并截取输入电压的有效数据片段所在区间youxiao，并确定输出稳态值y0
 youxiao=3:78;
 y0=140;
 
@@ -68,46 +63,12 @@ end
 %在原始数据图像中检验输入和输出
 subplot(211);
 for i1=1:length(input)-1
-    plot(t3(i1:i1+1),[input(i1) input(i1)],'--g');
-    plot([t3(i1+1) t3(i1+1)],input(i1:i1+1),'--g');
+    plot(t3(i1:i1+1),[input(i1) input(i1)],'--r');
+    plot([t3(i1+1) t3(i1+1)],input(i1:i1+1),'--r');
 end
-subplot(212);plot(t3,output,'*');
+subplot(212);plot(t3,output,'r*');
 
 %% 将输入标准化到-1和1；将输出减去稳态值
 input=2*(input>5)-1;
 output=output-y0;
 save('data.mat','input','output');
-%% 系统辨识
-u_M=input;
-z=output';
-
-figure;
-hold on;
-plot(t3,output,'--*');
-for i1=1:length(input)-1
-    plot(t3(i1:i1+1),[input(i1) input(i1)],'r');
-    plot([t3(i1+1) t3(i1+1)],input(i1:i1+1),'r');
-end
-title('去除稳态值后的增量输入输出');
-xlabel('时间/s');
-legend('output','input');
-
-%脉冲响应辨识
-[g_k]=xgfxf(u_M',output,Np);
-figure;
-plot(deltat:deltat:deltat*length(g_k),g_k);
-title('脉冲响应辨识结果'),xlabel('时间/s');ylabel('g_k');
-grid;
-figure;
-n=Np+1:Np+Np;
-for i1=n
-    output1(i1-n(1)+1,1)=g_k'*input(i1-1:-1:i1-Np+1);
-end
-plot(t3(n),output(n),t3(n),output1);
-title('脉冲响应辨识重构结果'),xlabel('时间/s');ylabel('液面高度');
-legend('真实输出','重构输出');
-% %最小二乘辨识
-% [J_L1, N1 ,theta1 ,t1, z_p]=Ls(u_M,z,Np,10,0);%批量最小二乘（LS）
-% [J_L2, N2, theta2, t2, theta]=RLS(u_M,z,Np,10,0);%递推最小二乘（RLS）
-% plotresult(Np,J_L1,J_L2,t1,t2,z,z_p,theta);% 画图
-
